6f747dd644a51d934dfff213f943f0229a16adeb
Tzell <36787074+Tzelal@users.noreply.github.com>
2021-10-15 23:15:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,25 @@
+
 #include <Arduino.h>
 
-const int mF  = 27;
-const int mB  = 26;
-const int stp1 = 4;
-const int stp2 = 18;
-const int ldr = 12;
-const int relay = 14;
-const unsigned long eventInterval = 600000;
-unsigned long previousTime = 0;
+const int mF  = 0;
+const int mB  = 1;
+const int stp1 = 3;
+const int stp2 = 4;
+
 
 void setup()
 {
-  Serial.begin(112500);
-
   pinMode(mF, OUTPUT);
   pinMode(mB, OUTPUT);
   pinMode(stp1, OUTPUT);
   pinMode(stp2, OUTPUT);
-  pinMode(relay, OUTPUT);
-  pinMode(ldr, INPUT);
 }
 
 void loop()
 { 
-  unsigned long currentTime = millis();
+  
   int stop1 = digitalRead(stp1);
   int stop2 = digitalRead(stp2); 
-  int ldrVal = analogRead(ldr);
       
   if(stop1 == 1)
     {  
@@ -45,21 +38,4 @@ void loop()
     {      
       digitalWrite(mB, LOW);   
     }
-
-  if(currentTime - previousTime >= eventInterval)
-    { if(ldrVal > 1000)
-      {
-        digitalWrite(relay, LOW);
-        Serial.print("\n It's day time");
-      }
-      else
-      {
-        digitalWrite(relay, HIGH);
-        Serial.print("\nIt's night time");
-      }
-      previousTime = currentTime;
-    }
 }
-
-
-
